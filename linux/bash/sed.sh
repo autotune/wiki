947fce58518e1c244c9e4a835367c915efb934aa
Brian Adams <b@contrasting.org>
2015-04-08 08:58:48 -0500

    Added comments

--- linux/bash/sed.sh
+++ linux/bash/sed.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # add TITLE to output command [COMMENT]
-# for example ls -lha [LSLHA} [COMMENT]
-# and make it easy for python to parse
-# or to parse in grep. 
+# for example ls -lha [LSLHA] [COMMENT]
+# and make it easy for python to parse [COMMENT]
+# or to parse in grep [COMMENT]
+# grep COMMENT [COMMENT].
 
 function printcmd {
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
