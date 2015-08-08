dadd7505eded1d6f2a3a6571e7f6f1fc09d2433e
Brian Adams <brian.adams@rackspace.com>
2015-08-08 12:35:02 -0500

    Fixed ARP syntax

--- networking/arp.sh
+++ networking/arp.sh
@@ -1,6 +1,9 @@
 # arp converts destination ip addresse to mac address on source server
+# Address                  HWtype  HWaddress           Flags Mask            Iface
 
 show_arp(){
-    arp-ev
+    arp -ev
 }
 
+
+show_arp
